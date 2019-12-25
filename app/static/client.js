666014aed2d3881a3003c606f0e4c5675c8f3c6d
baski1978 <bhaskar.namile@gmail.com>
2019-12-25 14:09:16 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -9,7 +9,7 @@ function showPicked(input) {
   alert(input.files[0].name);
   var reader = new FileReader();
   reader.onload = function() {
-    el("image-picked").src = reader.readAsDataURL(input.files[0]);
+    el("image-picked").src = reader.result;
     el("image-picked").className = "no-display";
   };
   reader.readAsDataURL(input.files[0]);
