63e70f102781285dfc0d0e41522121bcb1e05b7b
baski1978 <bhaskar.namile@gmail.com>
2019-12-26 19:13:05 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -31,7 +31,7 @@ async def download_file(url, dest):
                 f.write(data)
 
 async def setup_learner():
-  logging.info('I told you so-4')
+    
   await download_file(export_file_url, path / export_file_name)
     try:
         learn = load_learner(path, export_file_name)
