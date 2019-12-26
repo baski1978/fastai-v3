0b79392a0aa777f0c48bda25f23c52c549e0ab93
baski1978 <bhaskar.namile@gmail.com>
2019-12-26 14:33:32 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -21,8 +21,7 @@ function analyze() {
   el("analyze-button").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
-    xhr.timeout=2000;
- xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,true)
+   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,true);
   // xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/predict/`,true);
   //  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,true);predict
 
